0fbc718005fd124bdc1efe516b0a847b148695b5
joshua_hill <joshuah923@gmail.comc:/Users/joshu/OneDrive/Desktop/hacker/CSS-The-Hacker-Times-Part-1/CSS-The-Hacker-Times-Part-1/index.html>
2024-02-10 16:30:02 -0800

    added routes in controllers file

--- index.js
+++ index.js
@@ -1,38 +1,18 @@
-require('dotenv').config();
-const router = require('express').Router();
-const db = require('../modles')
+const express = require("express")
+const db = require('./models')
 const app = express();
 
 
+app.get("/")
 
-// Controllers & Routes
+//config
+require('dotenv').config();
+
+
+
+//controllers
 app.use('/travel', require('./controllers/travel'));
 
-app.get('/', (req, res) => {
-     res.send('Home Page');
-});
 
-app.get('*', (req, res) => {
-     res.status(404).send('<h1>404 Page</h1>');
-});
 
-router.get('/', (req.res) => {
-     res.send('GET /places stub')
-})
-
-router.post('/', (req.res) => {
-     res.send('POST /places stub')
-})
-
-//calls databse
-router.get('/', (req,res) => {
-     db.Place.find()
-     .then((places) => {
-          res.render('places/index', { places })
-     })
-     .catch(err => {
-          console.log(err)
-          res.render('error404')
-     })
-})
 app.listen(process.env.PORT);
