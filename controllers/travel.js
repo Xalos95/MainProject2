3b229d692f6051ab7f1c51f02073e93cba5e0398
Crisila <crisilaaban@gmail.com>
2024-02-12 11:19:59 -0800

    start travel routes

--- controllers/travel.js
+++ controllers/travel.js
@@ -1,6 +1,8 @@
-const router = require('express').Router()
+const express = require('express');
+const router = express.Router();
 
 
+// 
 app.get('/', (req, res) => {
      res.send('Home Page');
 });
