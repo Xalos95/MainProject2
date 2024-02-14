51ec99bd21b314383ff1974db6e94ade187be8b1
eduardo puga <0470hdbutter@gmail.com>
2024-02-14 14:11:16 -0800

    Navbar Finished

--- jcjeztravel/src/components/Navbar.jsx
+++ jcjeztravel/src/components/Navbar.jsx
@@ -1,9 +1,31 @@
 import React from "react";
-
+import Navbarstyle from './CSS/navbar.css';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
-export default Navbar
+    <div className={Navbarstyle.navbar}>
+      <title>Navbar</title>
+      <header>
+        <img className="logo" src="images/airplane.png" alt="logo" />
+        <nav>
+          <ul className="nav__links">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Travel Details</a>
+            </li>
+            <li>
+              <a href="#">User Login</a>
+            </li>
+            <li>
+              <a href="#">User Receipt</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
