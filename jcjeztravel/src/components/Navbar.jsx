78951ed2943eb0fc3dc7c631447f47d753a47aa7
Crisila <crisilaaban@gmail.com>
2024-02-14 20:32:44 -0800

    fix logo

--- jcjeztravel/src/components/Navbar.jsx
+++ jcjeztravel/src/components/Navbar.jsx
@@ -1,35 +1,3 @@
-// import React from "react";
-// // import './CSS/navbar.css';
-
-// const Navbar = () => {
-//   return (
-//     <div>
-//       <title>Navbar</title>
-//       <header>
-//         <img className="logo" src="images/airplane.png" alt="logo" />
-//         <nav>
-//           <ul className="nav__links">
-//             <li>
-//               <a href="#">Home</a>
-//             </li>
-//             <li>
-//               <a href="#">Travel Details</a>
-//             </li>
-//             <li>
-//               <a href="#">User Login</a>
-//             </li>
-//             <li>
-//               <a href="#">User Receipt</a>
-//             </li>
-//           </ul>
-//         </nav>
-//       </header>
-//     </div>
-//   );
-// };
-
-// export default Navbar;
-
 import React from "react";
 import { Link } from "react-router-dom"; // Import Link from react-router-dom
 import "../CSS/navbar.css"; 
@@ -37,14 +5,14 @@ import "../CSS/navbar.css";
 const Navbar = () => {
   return (
     <header>
-       <img className="logo" src="C:\Users\0470h\MainProject2\jcjeztravel\public\images\logo.png" alt="logo" />
+       <img className="logo" src="/images/logo.png" alt="logo" /> 
       <nav>
         <ul className="nav__links">
           <li>
-            <Link to="/">Home</Link> {/* Use Link instead of anchor tags */}
+            <Link to="/">Home</Link> 
           </li>
           <li>
-            <Link to="/travel/:name">Travel Details</Link> {/* Adjust paths accordingly */}
+            <Link to="/travel/:name">Travel Details</Link>
           </li>
           <li>
             <Link to="/travel/login">Login</Link>
